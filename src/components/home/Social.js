d5a3d46954b9b7aced637ae063fd44cd090295d1
Dharani damodaran <dharanidamodaran12@gmail.com>
2024-08-26 19:21:23 +0530

    Updated code

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -4,13 +4,13 @@ import {  FiGithub, FiLinkedin, FiInstagram} from "react-icons/fi";
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="https://www.instagram.com/_dharani_damodaran_/?hl=en" className="home__social-icon" target="_blank">
+            <a href="https://www.instagram.com/_dharani_damodaran_/?hl=en" className="home__social-icon" target="_blank" rel="noreferrer">
                 <FiInstagram />
             </a>
-            <a href="https://github.com/dashboard" className="home__social-icon" target="_blank">
+            <a href="https://github.com/dashboard" className="home__social-icon" target="_blank" rel="noreferrer">
                 <FiGithub />
             </a>
-            <a href="https://www.linkedin.com/in/dharani-d-19a879257/" className="home__social-icon" target="_blank">
+            <a href="https://www.linkedin.com/in/dharani-d-19a879257/" className="home__social-icon" target="_blank" rel="noreferrer">
                 <FiLinkedin />
             </a>
         </div> 
--- src/components/scrollup/ScrollUp.js
+++ src/components/scrollup/ScrollUp.js
@@ -10,7 +10,7 @@ const ScrollUp = () => {
     });
   
     return (
-    <a href="#" className="scrollup"><HiOutlineArrowSmUp className="scrollup__icon" rel="noreferrer"/></a>
+    <a href="/" className="scrollup"><HiOutlineArrowSmUp className="scrollup__icon" rel="noreferrer"/></a>
   );
 }
 
